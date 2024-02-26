35805aebd381aa8fab41e0eb8e76e25b877bcf2d
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-02-26 11:37:45 +0100

    Try to fix tests on Windows

--- test/test_file_creation_cool.py
+++ test/test_file_creation_cool.py
@@ -22,46 +22,44 @@ pytestmark = pytest.mark.parametrize(
 
 
 class TestClass:
-    def test_file_creation_thin_pixel(self, file, resolution):
+    def test_file_creation_thin_pixel(self, file, resolution, tmpdir):
         f = hictkpy.File(file, resolution)
 
         df = f.fetch(join=False).to_df()
         expected_sum = df["count"].sum()
 
-        with tempfile.TemporaryDirectory() as tmpdir:
-            path = os.path.join(tmpdir, "test.cool")
-            w = hictkpy.cooler.FileWriter(path, f.chromosomes(), f.resolution())
+        path = os.path.join(tmpdir, "test.cool")
+        w = hictkpy.cooler.FileWriter(path, f.chromosomes(), f.resolution())
 
-            chunk_size = 1000
-            for start in range(0, len(df), chunk_size):
-                end = start + chunk_size
-                w.add_pixels(df[start:end])
+        chunk_size = 1000
+        for start in range(0, len(df), chunk_size):
+            end = start + chunk_size
+            w.add_pixels(df[start:end])
 
-            w.finalize()
-            del w
-            gc.collect()
+        w.finalize()
+        del w
+        gc.collect()
 
-            f = hictkpy.File(path, resolution)
-            assert f.fetch().sum() == expected_sum
+        f = hictkpy.File(path, resolution)
+        assert f.fetch().sum() == expected_sum
 
-    def test_file_creation(self, file, resolution):
+    def test_file_creation(self, file, resolution, tmpdir):
         f = hictkpy.File(file, resolution)
 
         df = f.fetch(join=True).to_df()
         expected_sum = df["count"].sum()
 
-        with tempfile.TemporaryDirectory() as tmpdir:
-            path = os.path.join(tmpdir, "test.cool")
-            w = hictkpy.cooler.FileWriter(path, f.chromosomes(), f.resolution())
+        path = os.path.join(tmpdir, "test.cool")
+        w = hictkpy.cooler.FileWriter(path, f.chromosomes(), f.resolution())
 
-            chunk_size = 1000
-            for start in range(0, len(df), chunk_size):
-                end = start + chunk_size
-                w.add_pixels(df[start:end])
+        chunk_size = 1000
+        for start in range(0, len(df), chunk_size):
+            end = start + chunk_size
+            w.add_pixels(df[start:end])
 
-            w.finalize()
-            del w
-            gc.collect()
+        w.finalize()
+        del w
+        gc.collect()
 
-            f = hictkpy.File(path, resolution)
-            assert f.fetch().sum() == expected_sum
+        f = hictkpy.File(path, resolution)
+        assert f.fetch().sum() == expected_sum
