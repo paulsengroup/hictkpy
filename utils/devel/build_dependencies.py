054b905d1267f594c90137e7ffc86ce3d31dbe53
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2025-10-24 15:28:57 +0200

    Update utils/devel/build_dependencies.py to simplify building deps on the CI

--- utils/devel/build_dependencies.py
+++ utils/devel/build_dependencies.py
@@ -11,8 +11,8 @@ import platform
 import shlex
 import shutil
 import subprocess as sp
-import tarfile
-from typing import Any, Dict
+import tempfile
+from typing import Any, Dict, List
 
 
 def make_cli() -> argparse.ArgumentParser:
@@ -66,7 +66,32 @@ def make_cli() -> argparse.ArgumentParser:
         help="Print the commands that would be executed if --dry-run was not specified, then exit.",
     )
 
+    cli.add_argument(
+        "--deploy",
+        action="store_true",
+        default=False,
+        help="Deploy dependencies under the output prefix folder.",
+    )
+
+    cli.add_argument(
+        "--ci",
+        action="store_true",
+        default=False,
+        help="Install dependencies as required by the CI pipeline.",
+    )
+
+    cli.add_argument(
+        "--no-update",
+        action="store_true",
+        default=False,
+        help="Do not pass --update to conan install/create.",
+    )
+
     return cli
+
+
+def get_tempdir() -> pathlib.Path:
+    return pathlib.Path(tempfile.gettempdir())
 
 
 def infer_root_dir() -> pathlib.Path:
@@ -85,6 +110,23 @@ def run_or_print(args, env: Dict[str, str], dry_run: bool):
         sp.check_call(args, env=env)
 
 
+def add_output_params(args: List, dest: pathlib.Path, deploy: bool):
+    if deploy:
+        return args + [
+            f"--deployer-folder",
+            dest,
+            "--deployer=full_deploy",
+            "--conf=tools.deployer:symlinks=False",
+            "--output-folder",
+            dest / "cmake",
+        ]
+
+    return args + [
+        "--output-folder",
+        dest,
+    ]
+
+
 def run_conan(
     profile: str,
     build_type: str,
@@ -92,6 +134,8 @@ def run_conan(
     cppstd: str,
     output_prefix: pathlib.Path,
     dry_run: bool,
+    no_update: bool,
+    deploy: bool,
 ):
     output_folder = output_prefix / profile / build_type / ("shared" if shared else "static")
 
@@ -107,11 +151,14 @@ def run_conan(
         f"build_type={build_type}",
         "--settings",
         f"compiler.cppstd={cppstd}",
-        "--output-folder",
-        output_folder,
         "--options",
         f"*/*:shared={shared}",
     ]
+
+    if not no_update:
+        args.append("--update")
+
+    args = add_output_params(args, output_folder, deploy)
 
     env = os.environ.copy()
     env["CC"] = profile
@@ -139,6 +186,9 @@ def run_local(args: Dict[str, Any]):
     else:
         shared_build = [True, False]
 
+    no_update = args["no_update"]
+    deploy = args["deploy"]
+
     dry_run = args["dry_run"]
 
     output_prefix = infer_root_dir() / "conan-envs"
@@ -153,14 +203,177 @@ def run_local(args: Dict[str, Any]):
             *args,
             cppstd=cppstd,
             output_prefix=output_prefix,
+            deploy=deploy,
             dry_run=dry_run,
+            no_update=no_update,
         )
+
+
+def run_conan_profile_detect_ci(
+    name: str = "default",
+    cc: str | None = None,
+    cxx: str | None = None,
+    libcxx: str | None = None,
+) -> pathlib.Path:
+    assert "CONAN_HOME" in os.environ
+
+    args = ["conan", "profile", "detect", "--name", name]
+
+    env = os.environ.copy()
+    if cc is not None:
+        env["CC"] = cc
+    if cxx is not None:
+        env["CXX"] = cxx
+
+    run_or_print(args, env=env, dry_run=False)
+
+    profile = pathlib.Path(os.getenv("CONAN_HOME")) / "profiles" / name
+    if any(x is not None for x in (cc, cxx, libcxx)):
+        data = []
+        for line in profile.open("r"):
+            if line.startswith("compiler.libcxx"):
+                continue
+            data.append(line)
+
+        data.append("[buildenv]")
+        if cc is not None:
+            data.append(f"CC={cc}")
+        if cxx is not None:
+            data.append(f"CXX={cxx}")
+
+        profile.write_text("\n".join(data))
+
+    return profile
+
+
+def run_conan_install_b2_ci(version: str = "5.3.1"):
+    args = ["conan", "install", "--requires", f"b2/{version}", "--build=*"]
+    if platform.system() == "Linux":
+        args.extend(("--profile:b=gcc", "--profile:h=gcc"))
+
+    run_or_print(args, env=os.environ.copy(), dry_run=False)
+
+
+def run_ci_linux(args: Dict[str, Any]) -> str:
+    run_conan_profile_detect_ci(name="gcc", cc="gcc", cxx="g++", libcxx="libstdc++11")
+    run_conan_install_b2_ci()
+
+    cppstd = args["cppstd"]
+    conan_args = [
+        "conan",
+        "install",
+        "conanfile.py",
+        "--build",
+        "*",
+        "--profile:b=clang",
+        "--profile:h=clang",
+        "--settings",
+        f"compiler.cppstd={cppstd}",
+        "--settings",
+        f"build_type=Release",
+        "--options=*/*:shared=False",
+    ]
+
+    run_conan_profile_detect_ci(name="clang", cc="clang", cxx="clang++", libcxx="libstdc++11")
+
+    dest = get_tempdir() / "deps" / "cmake"
+    conan_args = add_output_params(conan_args, dest.parent, True)
+    run_or_print(conan_args, os.environ.copy(), False)
+
+    return str(dest)
+
+
+def run_ci_macos(args: Dict[str, Any]) -> str:
+    run_conan_profile_detect_ci(name="default")
+
+    cppstd = args["cppstd"]
+    conan_args = [
+        "conan",
+        "install",
+        "conanfile.py",
+        "--build",
+        "missing",
+        "--profile:b",
+        "default",
+        "--profile:h",
+        "default",
+        "--settings",
+        f"compiler.cppstd={cppstd}",
+        "--settings",
+        "build_type=Release",
+        "--settings:h",
+        "os.version=14.0",
+    ]
+
+    dest = get_tempdir() / "deps" / "cmake"
+    conan_args = add_output_params(conan_args, dest.parent, True)
+    run_or_print(conan_args, os.environ.copy(), False)
+
+    return str(dest)
+
+
+def run_ci_windows(args: Dict[str, Any]) -> str:
+    winsdk_version = os.getenv("WINSDK_VERSION")
+    if winsdk_version is None:
+        raise RuntimeError("Unable to read Windows SDK version from environment variable WINSDK_VERSION")
+
+    run_conan_profile_detect_ci(name="default")
+    run_conan_install_b2_ci()
+
+    cppstd = args["cppstd"]
+    conan_args = [
+        "conan",
+        "install",
+        "conanfile.py",
+        "--build",
+        "missing",
+        "--profile:b",
+        "default",
+        "--profile:h",
+        "default",
+        "--settings",
+        f"compiler.cppstd={cppstd}",
+        "--settings",
+        "compiler.runtime_type=Release",
+        "--settings",
+        "build_type=Release",
+        "--conf:a",
+        f"tools.microsoft:winsdk_version={winsdk_version}",
+    ]
+
+    dest = get_tempdir() / "deps" / "cmake"
+    conan_args = add_output_params(conan_args, dest.parent, True)
+    run_or_print(conan_args, os.environ.copy(), False)
+
+    return str(dest)
+
+
+def run_ci(args: Dict[str, Any]):
+    if "CONAN_HOME" not in os.environ:
+        raise RuntimeError("Please set the CONAN_HOME environment variable")
+
+    if platform.system() == "Linux":
+        run_ci_linux(args)
+        return
+
+    if platform.system() == "Darwin":
+        run_ci_macos(args)
+        return
+
+    if platform.system() == "Windows":
+        run_ci_windows(args)
+        return
+
+    raise RuntimeError(f"Platform {platform.system()} is not supported!")
 
 
 def main():
     args = vars(make_cli().parse_args())
 
-    run_local(args)
+    if args["ci"]:
+        run_ci(args)
+    else:
+        run_local(args)
 
 
 if __name__ == "__main__":
